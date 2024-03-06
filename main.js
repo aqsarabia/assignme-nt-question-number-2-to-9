7cedfc440bb72ca821f4ad83afad21a138e9ea8c
aqsarabia <160381125+aqsarabia@users.noreply.github.com>
2024-03-06 16:10:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+//Q2:
+var personname = "aqsa";
+console.log('hello', personname, "would you like to learn some typescript today");
+//Q3:
+var personName = "Aqsa";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log("aqsa\nAQSA\nAqsa");
+// Q4:
+console.log("'Allama Iqbal once said'", "Destiny is the prison and chain of the ignorant");
+// Q5:
+var famousperson = "Allama Iqbal";
+var message = "'once said,'Destiny is the prison and chain of ignorant'";
+console.log(famousperson, message);
+// Q6:
+var WhiteSpace = "\n\tAqsa Rabia\n\t";
+console.log(WhiteSpace);
+var withoutWhiteSpace = WhiteSpace.trim();
+console.log(WhiteSpace);
+// Q7:
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+// Q8:
+console.log(5 + 3);
+console.log(3 - 1);
+console.log(3 * 5);
+console.log(36 / 6);
+// Q9:
+var Favouritenumber = 3;
+console.log("'My favourite number is'", Favouritenumber);
